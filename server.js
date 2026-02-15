b7221352553b55a755881c6afd885b3a7848b140
GeorgiEnev <gogoenev@gmail.com>
2026-02-15 16:00:31 +0200

    detect end of HTTP headers using CRLF separator

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const net = require("net");
+const { buffer } = require("stream/consumers");
 const port = 6969;
 
 const server = net.createServer((socket) => {
     console.log("Client connected");   
-}
-);
+
+    let buffer = "";
+
+    socket.on("data", (chunk) => {
+        buffer += chunk.toString();
+
+        if (buffer.includes("\r\n\r\n")) {
+            console.log("Full request received");
+            console.log(buffer);
+
+            socket.end();
+        }
+    });
+});
 
 server.listen(port);
