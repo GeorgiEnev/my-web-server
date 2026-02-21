cb085ad537dc5e631013acb82ce25bb6bf6a578e
GeorgiEnev <gogoenev@gmail.com>
2026-02-21 15:23:07 +0200

    implement middleware system with centralized JSON validation

--- app.js
+++ app.js
@@ -2,6 +2,18 @@ import Router from "./router.js";
 
 const router = new Router();
 
+function validateJson(req, res, next) {
+  if (req.invalidJson) {
+    return res.status(400, "Bad Request").json({
+      error: "Invalid JSON body",
+    });
+  }
+
+  next();
+}
+
+router.use(validateJson);
+
 router.get("/", (req, res) => {
   res.send("Welcome to the Home Page");
 });
@@ -10,17 +22,7 @@ router.get("/about", (req, res) => {
   res.send("I Am Batman");
 });
 
-router.get("/search", (req, res) => {
-  res.send(JSON.stringify(req.query));
-});
-
 router.post("/api/echo", (req, res) => {
-  if (req.invalidJson) {
-    return res.status(400, "Bad Request").json({
-      error: "Invalid JSON body",
-    });
-  }
-
   res.json({ received: req.json });
 });
 
--- router.js
+++ router.js
@@ -1,6 +1,7 @@
 export default class Router {
   constructor() {
     this.routes = new Map();
+    this.middlewares = [];
   }
 
   get(path, handler) {
@@ -11,16 +12,30 @@ export default class Router {
     this.#addRoute("POST", path, handler);
   }
 
+  use(middlewares) {
+    this.middlewares.push(middlewares);
+  }
+
   handle(req, res) {
-    const methodRoutes = this.routes.get(req.method);
-    const handler = methodRoutes?.get(req.path);
+    let index = 0;
 
-    if (!handler) {
-      res.status(404, "Not Found").send("404 Not Found");
-      return;
-    }
+    const next = () => {
+      if (index < this.middlewares.length) {
+        const middleware = this.middlewares[index++];
+        return middleware(req, res, next);
+      }
 
-    handler(req, res);
+      const methodRoutes = this.routes.get(req.method);
+      const handler = methodRoutes?.get(req.path);
+
+      if (!handler) {
+        return res.status(404, "Not Found").send("404 Not Found");
+      }
+
+      handler(req, res);
+    };
+
+    next();
   }
 
   #addRoute(method, path, handler) {
