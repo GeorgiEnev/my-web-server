4644d817ab9e05f974fcca88310b7d061a2782da
GeorgiEnev <gogoenev@gmail.com>
2026-02-26 21:43:19 +0200

    remove temporary crash and early-response test routes

--- app/app.js
+++ app/app.js
@@ -7,33 +7,12 @@ const router = new Router();
 router.use(staticFiles("public"));
 router.use(validateJson);
 
-router.get("/", (req, res) => {
-  res.send("Welcome to the Home Page");
-});
-
 router.get("/about", (req, res) => {
   res.send("I Am Batman");
 });
 
 router.post("/api/echo", (req, res) => {
   res.json({ received: req.json });
-});
-
-router.get("/crash", () => {
-  throw new Error("Test crash");
-});
-
-function earlyResponse(req, res, next) {
-  if (req.path === "/early") {
-    return res.send("Early response");
-  }
-  next();
-}
-
-router.use(earlyResponse);
-
-router.get("/early", (req, res) => {
-  res.send("This should not appear");
 });
 
 router.get("/users/:id", (req, res) => {
